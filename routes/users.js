91c0082b81c3d541c3d15ba5db92dfac2a2247c7
Rita Trindade da Cruz <ritatrcr@unisabana.edu.co>
2024-09-18 12:15:25 -0500

    Fix users

--- routes/users.js
+++ routes/users.js
@@ -9,9 +9,29 @@ router.get('/', (req, res) => {
 });
 
 // Create User
+// Create products
 router.post('/', (req, res) => {
-  const newUser = { id: Date.now().toString(), ...req.body };
-  users.push(newUser);
+  const { id, name, email, age } = req.body;
+
+  // Validate JSON format
+  if (typeof id !== 'string' || typeof name !== 'string' || typeof email !== 'string' || typeof age !== 'string') {
+      return res.status(400).json({ message: 'Invalid JSON format' });
+  }
+
+  // Validate ID
+  const idAsNumber = parseInt(id, 10);
+  if (isNaN(idAsNumber) || idAsNumber <= 0 || products.some(p => p.id === id)) {
+      return res.status(400).json({ message: 'ID must be a unique positive integer' });
+  }
+
+  // Validate age
+  const ageAsNumber = parseInt(age);
+  if (isNaN(ageAsNumber) || ageAsNumber < 0) {
+      return res.status(400).json({ message: 'age must be a non-negative number' });
+  }
+
+  const newUser = { id, name, price, category };
+  products.push(newUser);
   res.status(201).json(newUser);
 });
 
@@ -24,11 +44,27 @@ router.get('/:id', (req, res) => {
 
 // Update User
 router.put('/:id', (req, res) => {
-  const index = users.findIndex(u => u.id === req.params.id);
-  if (index === -1) return res.status(404).json({ message: 'User not found' });
+  const userIndex = users.findIndex((p) => p.id === req.params.id);
 
-  users[index] = { id: req.params.id, ...req.body };
-  res.json(users[index]);
+  //Validate ID exist
+  if (userIndex === -1) {
+      return res.status(404).json({ message: 'User not found' });
+  }
+
+  // Validate JSON format
+  const { name, email, age } = req.body;
+  if (typeof name !== 'string' || typeof email !== 'string' || typeof age !== 'string') {
+      return res.status(400).json({ message: 'Invalid JSON format' });
+  }
+
+  // Validate price
+  const ageAsNumber = parseInt(price);
+  if (isNaN(ageAsNumber) || ageAsNumber < 0) {
+      return res.status(400).json({ message: 'Price must be a non-negative number' });
+  }
+
+  users[userIndex] = { id: req.params.id, name, price, category };
+  res.json(users[userIndex]);
 });
 
 // Delete user
